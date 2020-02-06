46c6646a52565cbc642680dfb877ba96e1b632d3
Phil Owen <phillipsowen@outlook.com>
2020-02-06 12:29:13 -0500

    somewhat working version.

--- QRW/server.py
+++ QRW/server.py
@@ -21,9 +21,25 @@ a couple json segments to test the request and response
 
 request:
 
+    {
+        "asynchronous": "false",
+        "bypass_cache": "true",
+        "max_results": 10,
+        "page_number": 1,
+        "query_message":
+        {
+            "original_question": "A question?"
+        }
+    }
 
+{"asynchronous":"false","bypass_cache":"true","max_results":10,"page_number":1,"query_message":{"original_question":"A question?"}}
+    
+    
 response:
 
+    {
+        ?
+    }
 
 """
 
@@ -40,29 +56,33 @@ async def get_handler(request):
         spec = yaml.load(f, Loader=yaml.SafeLoader)
 
     # load the query specification
-    query_validate = spec["components"]["schemas"]["Query"]
+    schema = spec["components"]["schemas"]["Query"]
 
     try:
+        # load the input into a json object
+        incoming = json.loads(request.args['query'][0])
+
         # validate the incoming json against the spec
-        jsonschema.validate(request.json, query_validate)
+        jsonschema.validate(instance=incoming, schema=schema)
 
     # all validation errors are manifested as a thrown exception
-    except jsonschema.exceptions.ValidationError as error:
+    except Exception as error:
+        # jsonschema.exceptions.ValidationError as error:
         # print (f"ERROR: {str(error)}")
         return response.json({'Query_failed_validation_message': str(error)}, status=400)
 
     # do the real work here. get a list of rewritten questions related to the requested one
-    query_rewritten = json.dumps({})
+    query_rewritten = json.loads('{}')
 
     # load the query response specification
     rewrite_validate = spec["components"]["schemas"]["Result"]
 
     try:
         # validate what is to be returned to insure it is also conforms to the Translator spec
-        jsonschema.validate(request.json, rewrite_validate)
+        # jsonschema.validate(request.json, rewrite_validate)
 
         # if we are here the response validated properly
-        return response.json(query_rewritten.json, status=200)
+        return response.json("Success", status=200)
 
     # all validation errors are manifested as a thrown exception
     except jsonschema.exceptions.ValidationError as error:
