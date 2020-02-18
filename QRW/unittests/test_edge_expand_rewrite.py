ae236e018b547076959885b3d515a13a76472c12
cbizon <bizon@renci.org>
2020-02-18 11:19:56 -0500

    Starting Edge Expansion

---
+++ QRW/unittests/test_edge_expand_rewrite.py
@@ -0,0 +1,29 @@
+import pytest
+import QRW.edge_expand_rewrite as rw
+from collections import defaultdict
+
+def write_testq(nodelist,predlist,directionlist):
+    querygraph = {}
+    nodes = []
+    edges = []
+    for i,nt in enumerate(nodelist):
+        nodes.append( {'id': f'node_{i}', 'type':nt} )
+    for i,p in enumerate(predlist):
+        if directionlist[i]:
+            edges.append( {'id': f'edge_{i}', 'type': p, 'source_id': f'node_{i}', 'target_id': f'node_{i+1}'})
+        else:
+            edges.append( {'id': f'edge_{i}', 'type': p, 'source_id': f'node_{i+1}', 'target_id': f'node_{i}'})
+    querygraph['nodes'] = nodes
+    querygraph['edges'] = edges
+    return querygraph
+
+def test_simple_sim():
+    tq = write_testq(['chemical_substance','disease'], ['treats'], [True])
+    print(tq)
+    nqs = rw.egde_expand(tq)
+    #I should get back at least a few new questions, and they should each have 3 nodes and 2 edges and go
+    # chemical-[x]-(?)-[y]-disease
+    assert len(nqs) > 3
+    for nq in nqs:
+        assert len(nq['nodes']) == 3
+        assert len(nq['edges']) == 2
