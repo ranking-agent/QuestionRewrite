9b4f7d121b03a4278d5621551d6031ae8aa1799a
Phil Owen <phillipsowen@outlook.com>
2020-02-24 10:09:01 -0500

    fix requirements.txt

--- src/server.py
+++ src/server.py
@@ -18,8 +18,55 @@ app.config.ACCESS_LOG = False
 app.blueprint(apidocs_blueprint)
 
 
-@app.post('/query')
-async def query_handler(request: Request) -> json:
+@app.post('/Node_Expand')
+async def node_expand_handler(request: Request) -> json:
+    """ Handler for question rewrite operations. """
+
+    # get the location of the Translator specification file
+    dir_path: str = os.path.dirname(os.path.realpath(__file__))
+
+    # load the Translator specification
+    with open(os.path.join(dir_path, 'translator_interchange_0.9.0.yaml')) as f:
+        spec: dict = yaml.load(f, Loader=yaml.SafeLoader)
+
+    # load the query specification, first get the question node
+    validate_with: dict = spec["components"]["schemas"]["Question"]
+
+    # then get the components in their own array so the relative references are found
+    validate_with["components"] = spec["components"]
+
+    # remove the question node because we already have it at the top
+    validate_with["components"].pop("Question", None)
+
+    try:
+        # load the input into a json object
+        incoming: dict = json.loads(request.body)
+
+        # validate the incoming json against the spec
+        jsonschema.validate(instance=incoming, schema=validate_with)
+
+    # all JSON validation errors are manifested as a thrown exception
+    except jsonschema.exceptions.ValidationError as error:
+        # print (f"ERROR: {str(error)}")
+        return response.json({'Question failed validation. Message': str(error)}, status=400)
+
+    # TODO: do the real work here. get a list of rewritten questions related to the requested one
+    query_rewritten: list = [incoming, incoming]
+
+    try:
+        # validate each response item against the spec
+        for item in query_rewritten:
+            jsonschema.validate(item, validate_with)
+
+    # all JSON validation errors are manifested as a thrown exception
+    except jsonschema.exceptions.ValidationError as error:
+        return response.json({'Response failed validation. Message': str(error)}, status=400)
+
+    # if we are here the response validated properly
+    return response.json(query_rewritten, status=200)
+
+@app.post('/Edge_Expand')
+async def edge_expand_handler(request: Request) -> json:
     """ Handler for question rewrite operations. """
 
     # get the location of the Translator specification file
